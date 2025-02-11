6a451ba80e083cd0f74ba0887c980fe858885c96
ItzELECTR0 <lazarmariotiberiu@gmail.com>
2025-02-11 02:12:40 +0200

    Testing: Reverse Customization changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,43 +3,11 @@
   import Cursor from '$lib/Cursor.svelte';
   import HamburgerMenu from '$lib/HamburgerMenu.svelte';
   import '/src/app.css';
-  import { onMount, onDestroy } from 'svelte';
-
-  let enableHamburger = true;
-  let enableCursorFollower = true;
-
-  function updatePreferences() {
-    if (typeof window !== 'undefined') {
-      const storedHamburger = localStorage.getItem('enableHamburger');
-      const storedCursor = localStorage.getItem('enableCursorFollower');
-      if (storedHamburger !== null) {
-        enableHamburger = storedHamburger === 'true';
-      }
-      if (storedCursor !== null) {
-        enableCursorFollower = storedCursor === 'true';
-      }
-    }
-  }
-
-  onMount(() => {
-    updatePreferences();
-    window.addEventListener('preferencesUpdated', updatePreferences);
-  });
-
-  onDestroy(() => {
-    if (typeof window !== 'undefined') {
-      window.removeEventListener('preferencesUpdated', updatePreferences);
-    }
-  });
 </script>
 
 <main>
   <TopNav />
-  {#if enableCursorFollower}
-    <Cursor />
-  {/if}
-  {#if enableHamburger}
-    <HamburgerMenu />
-  {/if}
+  <Cursor />
+  <HamburgerMenu />
   <slot />
 </main>
